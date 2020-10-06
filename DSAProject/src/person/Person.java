e040251c73f11d76f8f3619851a11d4e8b990306
TheBirb <mmjontxu@gmail.com>
2020-10-06 17:57:19 +0200

    Person class
    Person class basics done, no documentation done yet, packages socialNetwork and menu done for future development of those classes.

--- DSAProject/src/person/Person.java
+++ DSAProject/src/person/Person.java
@@ -1,5 +1,113 @@
 package person;
 
 public class Person {
+	//Attributes
+	private String id;
+	private String name;
+	private String surnames;
+	private String birthdate;
+	private String birthplace;
+	private String home;
+	private String studiedat;
+	private String workedat;
+	private String movies;
+	private String groupCode;
+	
+	
+	//class person
+	public Person(){
+		
+	}
+	//setters and getters
+	public String getId() {
+		return id;
+	}
 
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurnames() {
+		return surnames;
+	}
+
+	public void setSurnames(String surnames) {
+		this.surnames = surnames;
+	}
+
+	public String getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public String getBirthplace() {
+		return birthplace;
+	}
+
+	public void setBirthplace(String birthplace) {
+		this.birthplace = birthplace;
+	}
+
+	public String getHome() {
+		return home;
+	}
+
+	public void setHome(String home) {
+		this.home = home;
+	}
+
+	public String getStudiedat() {
+		return studiedat;
+	}
+
+	public void setStudiedat(String studiedat) {
+		this.studiedat = studiedat;
+	}
+
+	public String getWorkedat() {
+		return workedat;
+	}
+
+	public void setWorkedat(String workedat) {
+		this.workedat = workedat;
+	}
+
+	public String getMovies() {
+		return movies;
+	}
+
+	public void setMovies(String movies) {
+		this.movies = movies;
+	}
+
+	public String getGroupCode() {
+		return groupCode;
+	}
+
+	public void setGroupCode(String groupCode) {
+		this.groupCode = groupCode;
+	}
+	@Override
+	public String toString() {
+		
+		return "Id:"+this.id+","+"Name: "+this.name+","+"Surnames: "+this.surnames+","+"Birthdate: "+this.birthdate+","+"Home: "+this.home+","+"Studied at: "+this.studiedat+","+"Worked at:"+this.workedat+","+"Movies: "+this.movies+","+"GroupCode: "+this.groupCode;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
